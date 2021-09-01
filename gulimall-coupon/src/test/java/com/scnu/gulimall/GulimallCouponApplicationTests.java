4bbc84440efded3da38f84b942583e6f998437b4
xuhaoyao <623834276@qq.com>
2021-09-01 21:58:06 +0800

    项目初始化构建

---
+++ gulimall-coupon/src/test/java/com/scnu/gulimall/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scnu.gulimall;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/scnu/gulimall/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scnu.gulimall;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
